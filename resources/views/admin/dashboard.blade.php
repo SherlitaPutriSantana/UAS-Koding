3373826edaf267c59742e3c1ee5ba8376d99444d
SherlitaPutriSantana <sherlitaputri773@gmail.com>
2023-06-19 18:37:41 +0700

    UMKM-Food-and-Beverage
    UAS Pengkodean dan Pemrograman

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container-fluid">
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">UMKM Food and Beverage</h1>
+    </div>
+
+<!-- Content Row -->
+    <div class="row">
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-md-3">
+            <div class="card border-left-dark shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                               Pengguna</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $users }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-user fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-md-3">
+            <div class="card border-left-dark shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Transaksi</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transactions }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-euro-sign fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	
+	
+	
+
+
+</div>
+@endsection
